94dc9da4525f114526057b5da7c7aeb76f36686f
Марина Иванова <mvivanova_2@edu.hse.ru>
2020-05-07 04:39:10 +0500

    konva

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,27 +1,39 @@
-import React, {Fragment, useEffect, useRef, useState} from 'react'
-import Konva, {Transformer} from 'konva';
-import {Stage, Layer, Image, Rect} from 'react-konva';
+import React, {useRef, useState} from 'react'
+import {Image, Layer, Stage} from 'react-konva';
 import useImage from 'use-image';
+import 'react-light-accordion/demo/css/index.css';
 
-import t1 from '../../resources/t-1.png';
 import './Editor.scss'
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import {
+    bbq,
+    bush_1,
+    bush_2,
+    bush_3,
+    bush_4,
+    bush_5,
+    bush_6,
+    bush_aster,
+    bush_blue,
+    bush_pink,
+    bush_rose_red,
+    bush_rose_white, bush_rose_yellow,
+    chair, conifer,
+    gazebo,
+    house_1,
+    house_2,
+    house_3, lake, object_1, object_2,
+    pool, stone_1, stone_2,
+    table_1,
+    table_2, tree, tree_fruit
+} from "../../resources/objects/DesignObjects";
 
 const URLImage = ({image, isSelected, onSelect, onChange}) => {
     const [img] = useImage(image.src);
-
-    const shapeRef = useRef();
-    const trRef = useRef();
-
-    useEffect(() => {
-        if (isSelected) {
-            // we need to attach transformer manually
-            trRef.current.setNode(shapeRef.current);
-            trRef.current.getLayer().batchDraw();
-        }
-    }, [isSelected]);
-
     return (
-        <Fragment>
         <Image
             id={image.id}
             image={img}
@@ -29,134 +41,124 @@ const URLImage = ({image, isSelected, onSelect, onChange}) => {
             y={image.y}
             height={image.height}
             width={image.width}
-            {...image}
-            draggable
-
-            offsetX={img && image.width? image.width / 2 : 0}
-            offsetY={img && image.height? image.height / 2 : 0}
-            onClick={onSelect}
-            onTap={onSelect}
-            ref={shapeRef}
+            offsetX={img && image.width ? image.width / 2 : 0}
+            offsetY={img && image.height ? image.height / 2 : 0}
             onDragMove={e => {
                 e.target.x(Math.round(e.target.x() / 10) * 10);
                 e.target.y(Math.round(e.target.y() / 10) * 10);
             }}
-            onDragEnd={e => {
-                onChange({
-                    ...image,
-                    x: e.target.x(),
-                    y: e.target.y()
-                });
-            }}
-            onTransformEnd={e => {
-                // transformer is changing scale of the node
-                // and NOT its width or height
-                // but in the store we have only width and height
-                // to match the data better we will reset scale on transform end
-                const node = shapeRef.current;
-                const scaleX = node.scaleX();
-                const scaleY = node.scaleY();
-
-                // we will reset it back
-                node.scaleX(1);
-                node.scaleY(1);
-                onChange({
-                    ...image,
-                    x: node.x(),
-                    y: node.y(),
-                    width: Math.max(5, node.width() * scaleX),
-                    height: Math.max(node.height() * scaleY)
-                });
-            }}
+            draggable
         />
-            {isSelected && (
-                <Transformer
-                    ref={trRef}
-                />
-            )}
-        </Fragment>
     );
 };
 
-const images = [];
-let idCount = 0;
 export const Editor = () => {
-    const dragUrl = useRef();
-    const stageRef = useRef();
-    const [images, setImages] = useState([]);
-    const [selectedId, selectShape] = useState(null);
-
-    const checkDeselect = e => {
-        // deselect when clicked on empty area
-        const clickedOnEmpty = e.target === e.target.getStage();
-        if (clickedOnEmpty) {
-            selectShape(null);
-        }
+    //accordion
+    const [expanded, setExpanded] = React.useState(false);
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
     };
 
-
-
+    //d&d
+    const dragImg = useRef();
+    const stageRef = useRef();
+    const [design, setDesign] = useState([]);
     const onDragStart = e => {
-
-        dragUrl.current = {
+        dragImg.current = {
             src: e.target.src,
             height: e.target.height,
             width: e.target.width
         };
     };
+    const onDrop = e => {
+        stageRef.current.setPointersPositions(e);
+        setDesign(design.concat([
+                {
+                    ...stageRef.current.getPointerPosition(),
+                    src: dragImg.current.src,
+                    height: dragImg.current.height,
+                    width: dragImg.current.width
+                }
+            ])
+        );
+    }
 
     return (
-        <div>
-            <img
-                src={t1}
-                height="100"
-                width="100"
-                onDragStart={e=>onDragStart(e)}
+        <div className="editor">
+            <div className="editor__items-list">
+                <ExpansionPanel id="p-1" expanded={expanded === 'buildings'} onChange={handleChange('buildings')}>
+                    <ExpansionPanelSummary expandIcon={<span>🏡</span>}>Жилые постройки</ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <img src={house_1} height="300" onDragStart={e => onDragStart(e)}/>
+                        <img src={house_2} height="300" onDragStart={e => onDragStart(e)}/>
+                        <img src={house_3} height="300" onDragStart={e => onDragStart(e)}/>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+                <ExpansionPanel id="p-2" expanded={expanded === 'rest-zone'} onChange={handleChange('rest-zone')}>
+                    <ExpansionPanelSummary expandIcon={<span>🧩</span>}>Зона отдыха</ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <img src={bbq} height="70" onDragStart={e => onDragStart(e)}/>
+                        <img src={chair} height="80" onDragStart={e => onDragStart(e)}/>
+                        <img src={gazebo} height="125" onDragStart={e => onDragStart(e)}/>
+                        <img src={pool} height="125" onDragStart={e => onDragStart(e)}/>
+                        <img src={table_1} height="100" onDragStart={e => onDragStart(e)}/>
+                        <img src={table_2} height="100" onDragStart={e => onDragStart(e)}/>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+                <ExpansionPanel id="p-3" expanded={expanded === 'trees'} onChange={handleChange('trees')}>
+                    <ExpansionPanelSummary expandIcon={<span>🌳</span>}>Деревья</ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <img src={tree} height="130" onDragStart={e => onDragStart(e)}/>
+                        <img src={conifer} height="150" onDragStart={e => onDragStart(e)}/>
+                        <img src={tree_fruit} height="130" onDragStart={e => onDragStart(e)}/>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+                <ExpansionPanel id="p-4" expanded={expanded === 'bushes'} onChange={handleChange('bushes')}>
+                    <ExpansionPanelSummary expandIcon={<span>🌾</span>}>Кустарники</ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <img src={bush_1} height="90" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_2} height="90" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_3} height="90" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_4} height="90" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_5} height="90" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_6} height="90" onDragStart={e => onDragStart(e)}/>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+                <ExpansionPanel id="p-6" expanded={expanded === 'flowers'} onChange={handleChange('flowers')}>
+                    <ExpansionPanelSummary expandIcon={<span>🌺</span>}>Цветы</ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <img src={bush_aster} height="80" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_blue} height="80" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_pink} height="80" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_rose_red} height="80" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_rose_white} height="80" onDragStart={e => onDragStart(e)}/>
+                        <img src={bush_rose_yellow} height="80" onDragStart={e => onDragStart(e)}/>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+                <ExpansionPanel id="p-7" expanded={expanded === 'decorative-objects'} onChange={handleChange('decorative-objects')}>
+                    <ExpansionPanelSummary expandIcon={<span>🗿</span>}>Декоративные объекты</ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <img src={object_1} height="100" onDragStart={e => onDragStart(e)}/>
+                        <img src={object_2} height="100" onDragStart={e => onDragStart(e)}/>
+                        <img src={lake} height="150" onDragStart={e => onDragStart(e)}/>
+                        <img src={stone_1} height="30" onDragStart={e => onDragStart(e)}/>
+                        <img src={stone_2} height="30" onDragStart={e => onDragStart(e)}/>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
 
-            />
-            <img
-                src="https://konvajs.org/assets/lion.png"
-                height="50"
-                width="50"
-                onDragStart={e=>onDragStart(e)}
-            />
-            <div onDrop={e => {
-                stageRef.current.setPointersPositions(e);
-                setImages(images.concat([
-                        {
-                            ...stageRef.current.getPointerPosition(),
-                            id: idCount++,
-                            src: dragUrl.current.src,
-                            height: dragUrl.current.height,
-                            width: dragUrl.current.width
-                        }
-                    ])
-                );
-            }}
-                 onDragOver={e => e.preventDefault()}
-            >
+
+
+            <div className="editor__canvas" onDrop={e => onDrop(e)} onDragOver={e => e.preventDefault()}>
                 <Stage
-                    width={window.innerWidth}
-                    height={window.innerHeight}
-                    onMouseDown={checkDeselect}
-                    onTouchStart={checkDeselect}
-                    style={{border: '1px solid grey'}}
+                    width={750}
+                    height={400}
                     ref={stageRef}
                 >
                     <Layer>
-                        {images.map((image, i) => {
+                        {design.map((image) => {
                             return <URLImage
-                                key={i}
                                 image={image}
-                                isSelected={image.id === selectedId}
-                                onSelect={() => {
-                                    selectShape(image.id);
-                                }}
-                                onChange={newAttrs => {
-                                    let imgs = images.slice();
-                                    imgs[i] = newAttrs;
-                                    setImages(imgs);
-                                }}
                             />;
                         })}
                     </Layer>
@@ -165,3 +167,5 @@ export const Editor = () => {
         </div>
     )
 }
+
+
---
+++ src/resources/objects/DesignObjects.jsx
@@ -0,0 +1,74 @@
+import bbq_ from './bbq.png'
+
+import bush_1_ from './bush-1.png'
+import bush_2_ from './bush-2.png'
+import bush_3_ from './bush-3.png'
+import bush_4_ from './bush-4.png'
+import bush_5_ from './bush-5.png'
+import bush_6_ from './bush-6.png'
+
+import bush_aster_ from './bush-aster.png'
+import bush_blue_ from './bush-blue.png'
+import bush_pink_ from './bush-pink.png'
+import bush_rose_red_ from './bush-rose-red.png'
+import bush_rose_white_ from './bush-rose-white.png'
+import bush_rose_yellow_ from './bush-rose-yellow.png'
+
+import chair_ from './chair.png'
+import conifer_ from './conifer.png'
+import gazebo_ from './gazebo.png'
+
+import house_1_ from './house-1.png'
+import house_2_ from './house-2.png'
+import house_3_ from './house-3.png'
+
+import lake_ from './lake.png'
+import object_1_ from './object-1.png'
+import object_2_ from './object-2.png'
+
+import pool_ from './pool.png'
+
+import stone_1_ from './stone-1.png'
+import stone_2_ from './stone-2.png'
+
+import table_1_ from './table-1.png'
+import table_2_ from './table-2.png'
+
+import tree_ from './tree.png'
+import tree_fruit_ from './tree-fruit.png'
+
+export const bbq = bbq_;
+export const bush_1 = bush_1_;
+export const bush_2 = bush_2_;
+export const bush_3 = bush_3_;
+export const bush_4 = bush_4_;
+export const bush_5 = bush_5_;
+export const bush_6 = bush_6_;
+
+export const bush_aster = bush_aster_;
+export const bush_blue = bush_blue_;
+export const bush_pink = bush_pink_;
+export const bush_rose_red = bush_rose_red_;
+export const bush_rose_white = bush_rose_white_;
+export const bush_rose_yellow = bush_rose_yellow_;
+
+export const chair = chair_;
+export const conifer = conifer_;
+export const gazebo = gazebo_;
+export const house_1 = house_1_;
+export const house_2 = house_2_;
+export const house_3 = house_3_;
+
+export const lake = lake_;
+export const object_1 = object_1_;
+export const object_2 = object_2_;
+export const pool = pool_;
+export const stone_1 = stone_1_;
+export const stone_2 = stone_2_;
+export const table_1 = table_1_;
+export const table_2 = table_2_;
+
+export const tree = tree_;
+export const tree_fruit = tree_fruit_;
+
+
