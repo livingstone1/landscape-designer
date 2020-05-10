08bc61a8b2fe041bbca88dcb12d337032a4e8a7d
Марина Иванова <mvivanova_2@edu.hse.ru>
2020-05-11 02:06:16 +0500

    Updates

---
+++ precache-manifest.0b3230a818fd353bd6caec99ea3b4212.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61a547e7f92f60f4aa1612b37b1c957",
+    "url": "/landscape-designer/index.html"
+  },
+  {
+    "revision": "9ed670d41e20a939270a",
+    "url": "/landscape-designer/static/css/2.8cb4caa0.chunk.css"
+  },
+  {
+    "revision": "92200e6ed6888ace5520",
+    "url": "/landscape-designer/static/css/main.e738ddbc.chunk.css"
+  },
+  {
+    "revision": "9ed670d41e20a939270a",
+    "url": "/landscape-designer/static/js/2.9195f057.chunk.js"
+  },
+  {
+    "revision": "135ac833289b19367d5c0901c24259c2",
+    "url": "/landscape-designer/static/js/2.9195f057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92200e6ed6888ace5520",
+    "url": "/landscape-designer/static/js/main.566b4844.chunk.js"
+  },
+  {
+    "revision": "f53e422104b64345ebec",
+    "url": "/landscape-designer/static/js/runtime-main.e391aba3.js"
+  },
+  {
+    "revision": "1443cd10d395b8015d6c27b602b95954",
+    "url": "/landscape-designer/static/media/Merriweather Regular.1443cd10.ttf"
+  },
+  {
+    "revision": "2c95c879385ea2b4896ac068e03c0c24",
+    "url": "/landscape-designer/static/media/Merriweather Regular.2c95c879.eot"
+  },
+  {
+    "revision": "43f463ad73cda79e623d476d06fad210",
+    "url": "/landscape-designer/static/media/Merriweather Regular.43f463ad.woff2"
+  },
+  {
+    "revision": "6556f4a0b0dbf7d9d347e466ae75d142",
+    "url": "/landscape-designer/static/media/Merriweather Regular.6556f4a0.svg"
+  },
+  {
+    "revision": "aa827351a17623737172018e5bf7ebac",
+    "url": "/landscape-designer/static/media/Merriweather Regular.aa827351.woff"
+  },
+  {
+    "revision": "913dd150d32c7b5171e3533b808a6ac9",
+    "url": "/landscape-designer/static/media/bbq.913dd150.png"
+  },
+  {
+    "revision": "09b71857f9bcd8be9473cf5572e9c226",
+    "url": "/landscape-designer/static/media/bush-1.09b71857.png"
+  },
+  {
+    "revision": "c4d02d5f45236e8d8a595d02ff01ac54",
+    "url": "/landscape-designer/static/media/bush-2.c4d02d5f.png"
+  },
+  {
+    "revision": "db1da1e90e749ab6555eed6663505996",
+    "url": "/landscape-designer/static/media/bush-3.db1da1e9.png"
+  },
+  {
+    "revision": "7bcebeeac304bc697c74f7cfaf73e86a",
+    "url": "/landscape-designer/static/media/bush-4.7bcebeea.png"
+  },
+  {
+    "revision": "a7395221a04c30cc48f4a4322082e3c9",
+    "url": "/landscape-designer/static/media/bush-5.a7395221.png"
+  },
+  {
+    "revision": "c6d538de03579c691b028b80afc40b4c",
+    "url": "/landscape-designer/static/media/bush-6.c6d538de.png"
+  },
+  {
+    "revision": "e1375db85bdc5c138eb29e714384781c",
+    "url": "/landscape-designer/static/media/bush-aster.e1375db8.png"
+  },
+  {
+    "revision": "f025298637487ab6d227e78e7b90306d",
+    "url": "/landscape-designer/static/media/bush-blue.f0252986.png"
+  },
+  {
+    "revision": "81e20bc9f7fdf0f978cebd9e368094bd",
+    "url": "/landscape-designer/static/media/bush-pink.81e20bc9.png"
+  },
+  {
+    "revision": "b4751bee52e653a66ad6fed409b66b96",
+    "url": "/landscape-designer/static/media/bush-rose-red.b4751bee.png"
+  },
+  {
+    "revision": "ba7573afa57b7f8debb1630a8adc6d20",
+    "url": "/landscape-designer/static/media/bush-rose-white.ba7573af.png"
+  },
+  {
+    "revision": "3144a073d49d87be4f6eb9d796a2173b",
+    "url": "/landscape-designer/static/media/bush-rose-yellow.3144a073.png"
+  },
+  {
+    "revision": "8d66ff146e39b80b66d64504f607c892",
+    "url": "/landscape-designer/static/media/chair.8d66ff14.png"
+  },
+  {
+    "revision": "3147ae6e2bc0f774592ef634545facdc",
+    "url": "/landscape-designer/static/media/conifer.3147ae6e.png"
+  },
+  {
+    "revision": "130fb7e17ccfb4df5c1ba903201b90e9",
+    "url": "/landscape-designer/static/media/gazebo.130fb7e1.png"
+  },
+  {
+    "revision": "a111f6d8c4c0607d2e68833e8e09c93a",
+    "url": "/landscape-designer/static/media/house-1.a111f6d8.png"
+  },
+  {
+    "revision": "290b1c3f163e001956e1a16920e201d5",
+    "url": "/landscape-designer/static/media/house-2.290b1c3f.png"
+  },
+  {
+    "revision": "be7dad1b5e01b31cd86e77c854495902",
+    "url": "/landscape-designer/static/media/house-3.be7dad1b.png"
+  },
+  {
+    "revision": "fb6d28660073ca5d65a176020869a747",
+    "url": "/landscape-designer/static/media/lake.fb6d2866.png"
+  },
+  {
+    "revision": "22f645cab66a5e590d24ce7cbfa4bb0b",
+    "url": "/landscape-designer/static/media/logo.22f645ca.png"
+  },
+  {
+    "revision": "a1f5ed8befe206af7d9ba8b9396ca292",
+    "url": "/landscape-designer/static/media/mail.a1f5ed8b.png"
+  },
+  {
+    "revision": "3c13eef60407d0af7d2937f75bd1523d",
+    "url": "/landscape-designer/static/media/object-1.3c13eef6.png"
+  },
+  {
+    "revision": "54ae7d9977f4c323c3f282b428914557",
+    "url": "/landscape-designer/static/media/object-2.54ae7d99.png"
+  },
+  {
+    "revision": "df067943b45baa20192efe86bfd82c77",
+    "url": "/landscape-designer/static/media/pool.df067943.png"
+  },
+  {
+    "revision": "4352f12827ea372071e8ad01a5939dc6",
+    "url": "/landscape-designer/static/media/stone-1.4352f128.png"
+  },
+  {
+    "revision": "6f592f4e9f4d2635bd353546ef6dc75c",
+    "url": "/landscape-designer/static/media/stone-2.6f592f4e.png"
+  },
+  {
+    "revision": "9e5fd1fa818a1261e09cd7aa822bf744",
+    "url": "/landscape-designer/static/media/table-1.9e5fd1fa.png"
+  },
+  {
+    "revision": "24179a5e935d7d73f94d766737bca4c1",
+    "url": "/landscape-designer/static/media/table-2.24179a5e.png"
+  },
+  {
+    "revision": "5f5fad71803664bca134996cacbaaf07",
+    "url": "/landscape-designer/static/media/telegram.5f5fad71.png"
+  },
+  {
+    "revision": "64c22bee7f45a9957fedbbf712500b17",
+    "url": "/landscape-designer/static/media/tree-fruit.64c22bee.png"
+  },
+  {
+    "revision": "427f652b3682c099edf2fa2cec6648d3",
+    "url": "/landscape-designer/static/media/tree.427f652b.png"
+  }
+]);
